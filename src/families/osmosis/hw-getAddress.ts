99148d1812a3ca599910f69ca148334bfd1e7f32
Marco - Figment <98154129+marco-figment@users.noreply.github.com>
2022-02-17 14:52:25 +0100

    working hw-getAddress

--- src/families/osmosis/hw-getAddress.ts
+++ src/families/osmosis/hw-getAddress.ts
@@ -2,7 +2,6 @@ import Cosmos from "@ledgerhq/hw-app-cosmos";
 import type { Resolver } from "../../hw/getAddress/types";
 
 const resolver: Resolver = async (transport, { path, verify, currency }) => {
-  console.log("helloooo");
   const cosmos = new Cosmos(transport);
   const r = await cosmos.getAddress(path, currency.id, verify || false);
   return {
