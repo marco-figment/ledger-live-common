d813f45cfc419e403f26e5ac2881b8f726182ab5
Marco - Figment <98154129+marco-figment@users.noreply.github.com>
2022-02-25 17:40:59 +0100

    Terra-inspired isAddressValid

--- src/families/osmosis/utils.ts
+++ src/families/osmosis/utils.ts
@@ -1,28 +1,25 @@
-// import { decode } from "bech32";
+import { decode } from "bech32";
 
-// function checkPrefixAndLength(
-//   prefix: string,
-//   data: string,
-//   length: number
-// ): boolean {
-//   try {
-//     const vals = decode(data);
-//     return vals.prefix === prefix && data.length == length;
-//   } catch (e) {
-//     return false;
-//   }
-// }
+// Address validation freely inspired from
+// https://github.com/terra-money/terra.js/blob/9e5f553de3ff3e975eaaf91b1f06e45658b1a5e0/src/core/bech32.ts
+function checkPrefixAndLength(
+  prefix: string,
+  data: string,
+  length: number
+): boolean {
+  try {
+    const vals = decode(data);
+    return vals.prefix === prefix && data.length == length;
+  } catch (e) {
+    return false;
+  }
+}
 
-// // eslint-disable-next-line @typescript-eslint/no-namespace
-// export namespace AccAddress {
-//   /**
-//    * Checks if a string is a valid Terra account address.
-//    *
-//    * @param data string to check
-//    */
-//   export function validate(data: string): boolean {
-//     return checkPrefixAndLength("terra", data, 44);
-//   }
-// }
-
-// TODO will need to write validation for Osmosis address
+/**
+ * Checks if a string is a valid Osmosis account address.
+ *
+ * @param addr string to check
+ */
+export function isValidAddress(addr: string): boolean {
+  return checkPrefixAndLength("osmo", addr, 44);
+}
