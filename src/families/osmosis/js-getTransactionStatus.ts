80715455153397ab85c99fa6a34e566bde5a8af5
Marco - Figment <98154129+marco-figment@users.noreply.github.com>
2022-02-25 17:13:37 +0100

    Transaction Status

---
+++ src/families/osmosis/js-getTransactionStatus.ts
@@ -0,0 +1,70 @@
+import { BigNumber } from "bignumber.js";
+import {
+  NotEnoughBalance,
+  RecipientRequired,
+  InvalidAddress,
+  FeeNotLoaded,
+  AmountRequired,
+  InvalidAddressBecauseDestinationIsAlsoSource,
+} from "@ledgerhq/errors";
+import { Account, TransactionStatus } from "../../types";
+import { Transaction } from "./types";
+import { isValidAddress } from "./utils";
+
+const getTransactionStatus = async (
+  account: Account,
+  transaction: Transaction
+): Promise<TransactionStatus> => {
+  const errors: {
+    fees?: Error;
+    amount?: Error;
+    recipient?: Error;
+  } = {};
+  const warnings: any = {};
+  const useAllAmount = !!transaction.useAllAmount;
+
+  if (account.freshAddress === transaction.recipient) {
+    errors.recipient = new InvalidAddressBecauseDestinationIsAlsoSource();
+  }
+
+  if (!transaction.fees || !transaction.fees.gt(0)) {
+    errors.fees = new FeeNotLoaded();
+  }
+
+  const estimatedFees = transaction.fees || new BigNumber(0);
+
+  const amount = useAllAmount
+    ? account.balance.minus(estimatedFees)
+    : new BigNumber(transaction.amount);
+
+  if (amount.lte(0) && !transaction.useAllAmount) {
+    // shouldn't the first condition be valid always and not just when transaction.useAllAmount==true?
+    errors.amount = new AmountRequired();
+  }
+
+  const totalSpent = amount.plus(estimatedFees);
+
+  if (totalSpent.gt(account.balance)) {
+    errors.amount = new NotEnoughBalance();
+  }
+
+  if (!errors.amount && account.balance.lt(estimatedFees)) {
+    errors.amount = new NotEnoughBalance();
+  }
+
+  if (!transaction.recipient) {
+    errors.recipient = new RecipientRequired();
+  } else if (!isValidAddress(transaction.recipient)) {
+    errors.recipient = new InvalidAddress();
+  }
+
+  return Promise.resolve({
+    errors,
+    warnings,
+    estimatedFees,
+    amount,
+    totalSpent,
+  });
+};
+
+export default getTransactionStatus;
